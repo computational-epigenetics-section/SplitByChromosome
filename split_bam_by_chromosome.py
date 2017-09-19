f7e651cd97c716d6bf691395ecdb8ef0a332e83d
Scott <cas3@bcm.edu>
2017-09-19 11:10:40 -0500

    arg parser added

--- split_bam_by_chromosome.py
+++ split_bam_by_chromosome.py
@@ -20,15 +20,29 @@ import sys
 import subprocess
 import time
 import sys
+import argparse
 
-chromosomes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-               "11", "12", "13", "14", "15", "16", "17", "18", "19",
-               "20", "21", "22", "X", "Y"]
+parser = argparse.ArgumentParser()
+parser.add_argument("--chr", help="Set if input bam has chromosome IDs beginning with 'chr'", action="set_true")
+parser.add_argument("input_bam", "Input bam file to split")
 
-test = ["20", "21"]  # Use this for testing small scale
+args = parser.parse_args()
+
+if args.chr:
+    chromosomes = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+               "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19",
+               "chr20", "chr21", "chr22", "chrX", "chrY"]
+else:
+    chromosomes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                   "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                   "20", "21", "22", "X", "Y"]
+
+
+
+# test = ["20", "21"]  # Use this for testing small scale
 
 # Get the input file from the command line
-input_bam = sys.argv[1]
+input_bam = args.input_bam
 file_name = input_bam.split('.')[0]
 print("Input file is %s" % input_bam)
 sys.stdout.flush()  # This is used to force a write to stdout on slurm
