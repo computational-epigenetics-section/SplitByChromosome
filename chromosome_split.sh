178736bfa209f85f9c7f0d48fba95f748b6d572c
Anthony Scott <ascott329@gmail.com>
2017-10-10 08:48:49 -0500

    updated sbatch file to handle --chr flag

--- chromosome_split.sh
+++ chromosome_split.sh
@@ -2,4 +2,7 @@
 
 #SBATCH --time=16:00:00 -n24 -p bynode
 
-python3 split_by_chromosome.py $1
+#Args: Input_file.bam [--chr]
+# Use optional --chr if chromosome IDs start with 'chr'
+
+python3 /home/scott/jobs/scripts/split_bam_by_chromosome.py $1 $2
